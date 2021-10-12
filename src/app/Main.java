f9e11ee1fe8786ce507467229aaf6fc9dacd27dd
Moises Costa Caldas <sesiom.br@gmail.com>
2021-10-11 22:13:31 -0300

    HelloWorld

---
+++ src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+import java.io.PrintStream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrintStream out = new PrintStream(System.out);
+		out.print("Outro exemplo de helloworld");
+		
+	}
+
+}

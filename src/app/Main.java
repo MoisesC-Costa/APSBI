aa55be04ce386a18001ab16731964f0e0211b2cf
Moises Costa Caldas <sesiom.br@gmail.com>
2021-10-31 17:18:29 -0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,8 +4,7 @@ import java.io.PrintStream;
 public class Main {
 
 	public static void main(String[] args) {
-		PrintStream out = new PrintStream(System.out);
-		out.print("Outro exemplo de helloworld");
+		String fruta = "Banana";
 		
 	}
 

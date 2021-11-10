c0bd2b36e108fed16b6c59476efb18b9dc04c2b9
Moises Costa Caldas <sesiom.br@gmail.com>
2021-11-10 19:03:17 -0300

    Update LoginFrame.java

--- src/cliente/frames/LoginFrame.java
+++ src/cliente/frames/LoginFrame.java
@@ -4,13 +4,20 @@ import javax.swing.JFrame;
 
 
 public class LoginFrame extends JFrame implements Runnable {
+
+	public LoginFrame() {
+		this.setTitle("Inicializador de Sistema");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(700, 500);
+		this.setLocationRelativeTo(null);		
+		this.getContentPane().setLayout(null);
+		this.setResizable(false);
+
+	}
 	
 	@Override
 	public void run() {
-		this.setSize(800, 500);
-		this.getContentPane().setLayout(null);
-		this.setTitle("Inicializador de Sistema");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		
 		this.setVisible(true);
 		
